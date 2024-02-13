2f8170becc31c9b60b9a00734b9db422d1a25f76
snigbar <snigbar@outlook.com>
2024-02-13 17:37:10 +0600

    add feature section

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
+import Features from "../sections/Features";
 
 function Home() {
   return (
     <div>
       <Header></Header>
       <SearchBar></SearchBar>
+      <Features></Features>
     </div>
   );
 }
---
+++ frontend/src/sections/Features.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+import featureImg from "../assets/featureImg.jpg";
+import featureImg2 from "../assets/featureImg2.jpg";
+
+export default function Features() {
+  return (
+    <div className="py-8 md:py-12 my-8 w-11/12 lg:w-4/5 mx-auto flex flex-col lg:flex-row items-center justify-between">
+      <div className="w-full p-4 space-y-4 text-center lg:text-left">
+        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold tracking-wider md:leading-[3.5rem]">
+          Discover Top Hotels Worldwide
+        </h1>
+        <p className="text-sm md:text-lg">
+          Explore luxury escapes worldwide: from European palaces to Maldivian
+          hideaways, indulge in sophistication at your fingertips.
+        </p>
+        <button className="mt-4">
+          <Link
+            to={`/search`}
+            className="bg-indigo-600 text-white h-full px-3 py-2 font-semibold max-w-fit hover:bg-indigo-500"
+          >
+            Find Hotels
+          </Link>
+        </button>
+      </div>
+      <div className="w-full p-2 shadow-lg bg-white rounded-lg relative">
+        <img
+          src={featureImg}
+          alt="hotel feature image"
+          className="object-cover rounded-lg"
+        />
+
+        <div className="hidden md:block w-3/5 p-1 shadow-lg bg-white rounded-lg absolute -bottom-16 -right-16">
+          <img
+            src={featureImg2}
+            alt="hotel feature image"
+            className="object-cover rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
